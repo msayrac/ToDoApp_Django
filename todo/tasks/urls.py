e278fddc01d75fbe6a9b534aeabad3fd9f56af79
msayracode <msayracode@gmail.com>
2026-03-06 22:58:45 +0300

    update item added

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -3,8 +3,9 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="list"),
     path('index', views.index),
+    path('update_task/<str:pk>/', views.updateTask, name="update_task"),
 ]
 
 
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,11 +1,29 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from .forms import *
 
 # Create your views here.
 
 def index(request):
     tasks= Task.objects.all()
-    context= {'tasks':tasks}
+
+    form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context= {'tasks':tasks, 'form':form}
     # return HttpResponse('Hello index page')
     return render(request, 'tasks/list.html',context)
+
+def updateTask(request, pk):
+
+    item = Task.objects.get(id=pk)
+
+    context = {'item':item}
+
+    return render(request, 'tasks/update_task.html',context)
